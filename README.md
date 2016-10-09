ceabd2c31e8cc02591360df7423114603501cf41
Lauro <laurocdesa@gmail.com>
2016-10-09 13:17:46 -0300

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # django-docs.bat
 Django Documentation Menu for Windows
+=====================================
+
+Integrates usual commands and other tools for documentation to make admin's life easier on windows.
+
+Features:
+
+1. Integration with [Sphinx](http://www.sphinx-doc.org/en/stable/);
+1. Integration with [ResTView](https://mg.pov.lt/restview/); and
+1. Integration with [PyLit](http://pylit.berlios.de/).
---
+++ docs.bat
@@ -0,0 +1,54 @@
+﻿REM DJANGO DOCUMENTATION MENU FOR WINDOWS
+REM VERSION 1.0
+
+REM CLEANING OUTPUT
+@echo off
+ECHO OFF
+
+REM SAVING PyLit PATH
+SET PYLITPATH=S:\prg\py\pylit
+
+REM SAVING PYTHON PATH
+SET PYTHONPATH=S:\PortableApps\WinPython\python-3.4.4.amd64
+
+REM SAVING PATH
+SET PATH=%PYLITPATH%;%PYTHONPATH%;%PYTHONPATH%\Scripts;%PATH%
+
+REM SHOWING MENU
+
+:MENU
+CLS
+ECHO ########## MENU ##########
+ECHO # 1. SPHINX DOC          #
+ECHO # 2. ReST VIEW           #
+ECHO # 3. PyLit               #
+ECHO ##########################
+ECHO # 4. EXIT                #
+ECHO ##########################
+choice /C 1234 /M "SELECT AN OPTION:"
+
+IF %ERRORLEVEL% EQU 1 GOTO OPT1
+IF %ERRORLEVEL% EQU 2 GOTO OPT2
+IF %ERRORLEVEL% EQU 3 GOTO OPT3
+IF %ERRORLEVEL% EQU 4 GOTO OPT4
+
+:OPT1
+%PYTHONPATH%\Scripts\sphinx-apidoc.exe -o documentation ..
+CALL make.bat html
+PAUSE
+GOTO MENU
+
+:OPT2
+SET /P FILE=Which RST file would like to show?
+START %PYTHONPATH%\Scripts\restview.exe "%FILE%"
+PAUSE
+GOTO MENU
+
+:OPT3
+SET /P FILE=Which RST file would like to convert?
+%PYTHONPATH%\python.exe %PYLITPATH%\pylit.py "%FILE%" "%FILE:~0,-4%.py"
+PAUSE
+GOTO MENU
+
+:OPT4
+EXIT
